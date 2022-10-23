3d598206c72ba552f4e9b0b7e8fd7179fd84c61e
Finn O'Prey <finn@oprey.co>
2022-10-23 12:13:37 +1100

    Moved mongo database name into .env

--- src/lib/server/db.js
+++ src/lib/server/db.js
@@ -1,7 +1,7 @@
-import { MONGO_URI } from '$env/static/private'
+import { MONGO_URI, MONGO_DB_NAME } from '$env/static/private'
 import { MongoClient } from 'mongodb'
 
 const client = new MongoClient(MONGO_URI)
 await client.connect()
 
-export default client.db('guestbook')
+export default client.db(MONGO_DB_NAME)
